8f7381d9e58c2ba82b765801955b091d6d72dd01
Pavel-Shikhov <Pavel.Shikhov@outlook.com>
2020-01-30 14:51:37 +0300

    fixed Calculator

--- src/main/java/com/tsystems/javaschool/tasks/calculator/Calculator.java
+++ src/main/java/com/tsystems/javaschool/tasks/calculator/Calculator.java
@@ -1,11 +1,14 @@
 package com.tsystems.javaschool.tasks.calculator;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Stack;
-import java.util.Vector;
+import java.util.*;
 
 public class Calculator {
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+//        System.out.println(calculator.evaluate("5+41..1-6"));
+        System.out.println(calculator.evaluate("3/0"));
+    }
+
     /**
      * Evaluate statement represented as string.
      *
@@ -15,72 +18,65 @@ public class Calculator {
      * @return string value containing result of evaluation or null if statement is invalid
      */
     public String evaluate(String statement) {
-        char[] symbols = statement.toCharArray();
-        Stack<Double> numbers = new Stack<>();
-        Stack<Character> operations = new Stack<>();
-
-        for (int i = 0; i < symbols.length; i++) {
-            if (symbols[i] == ' '){
-                continue;
-                //TODO: unary +
-            } else if (symbols[i] >= '0' && symbols[i] <= '9'){
-                int numberOfDigits = 0;
-                boolean isDotPresent = false;
-                StringBuffer currentNumber = new StringBuffer();
-                while (i+numberOfDigits < symbols.length
-                        && (symbols[i+numberOfDigits] >= '0' && symbols[i+numberOfDigits] <= '9'
-                        || symbols[i+numberOfDigits] == '.')){
-                    if (isDotPresent == false && symbols[i+numberOfDigits] == '.'){
-                        currentNumber.append(symbols[i + numberOfDigits]);
-                        isDotPresent = true;
-                    } else {
-                        currentNumber.append(symbols[i + numberOfDigits]);
-                    }
-                    numberOfDigits++;
-                }
-                i += numberOfDigits - 1;
-                numbers.add(Double.valueOf(currentNumber.toString()));
-            //negative number parsing
-            } else
-//                if ( i + 1 < symbols.length && symbols[i] == '-' && symbols[i+1] > '0' && symbols[i+1] < '9' ) {
-//                StringBuffer currentNumber = new StringBuffer();
-//                currentNumber.append(symbols[i]);
-////                int possibleDigitIndex = i + 1;
-//                i++;
-//                while (i < symbols.length &&
-//                        symbols[i] >= '0' && symbols[i] <= '9'){
-//                    currentNumber.append(symbols[i++]);
-//                }
-//                numbers.push(Double.valueOf(currentNumber.toString()));
-//            } else
-                if (symbols[i] == '(') {
-                operations.push(symbols[i]);
-            } else if (symbols[i] == ')') {
-                while (operations.peek() != '('){
-                    numbers.push(compute(operations.pop(), numbers.pop(), numbers.pop()));
-                }
-                operations.pop();
-            } else if (symbols[i] == '+' || symbols[i] == '-' ||  symbols[i] == '*' || symbols[i] == '/') {
-                while (!operations.empty() && isOperationPrioritized(symbols[i], operations.peek())){
-                    numbers.push(compute(operations.pop(), numbers.pop(), numbers.pop()));
-                }
-                if (symbols[i] == '-'){
-                    operations.push('+');
-                }
-                operations.push(symbols[i]);
-            } else {
+        try {
+            if (statement == null || statement.isEmpty()){
                 return null;
             }
-        }
+            char[] symbols = statement.toCharArray();
+            Stack<Double> numbers = new Stack<>();
+            Stack<Character> operations = new Stack<>();
 
-        while (!operations.empty())
-            numbers.push(compute(operations.pop(), numbers.pop(), numbers.pop()));
-        double result = numbers.pop();
-        return result % 1 == 0 ? Integer.toString((int) result) : Double.toString(result);
+            for (int i = 0; i < symbols.length; i++) {
+                if (symbols[i] == ' '){
+                    continue;
+                } else if (symbols[i] >= '0' && symbols[i] <= '9'){
+                    int numberOfDigits = 0;
+                    boolean isDotPresent = false;
+                    StringBuffer currentNumber = new StringBuffer();
+                    while (i+numberOfDigits < symbols.length
+                            && (symbols[i+numberOfDigits] >= '0' && symbols[i+numberOfDigits] <= '9'
+                            || symbols[i+numberOfDigits] == '.')){
+                        if (isDotPresent == false && symbols[i+numberOfDigits] == '.'){
+                            currentNumber.append(symbols[i + numberOfDigits]);
+                            isDotPresent = true;
+                        } else {
+                            currentNumber.append(symbols[i + numberOfDigits]);
+                        }
+                        numberOfDigits++;
+                    }
+                    i += numberOfDigits - 1;
+                    numbers.add(Double.valueOf(currentNumber.toString()));
+                } else if (symbols[i] == '(') {
+                    operations.push(symbols[i]);
+                } else if (symbols[i] == ')') {
+                    while (operations.peek() != '('){
+                        numbers.push(compute(operations.pop(), numbers.pop(), numbers.pop()));
+                    }
+                    operations.pop();
+                } else if (symbols[i] == '+' || symbols[i] == '-' ||  symbols[i] == '*' || symbols[i] == '/') {
+                    while (!operations.empty() && isOperationPrioritized(symbols[i], operations.peek())){
+                        numbers.push(compute(operations.pop(), numbers.pop(), numbers.pop()));
+                    }
+                    operations.push(symbols[i]);
+                } else {
+                    return null;
+                }
+            }
+
+            while (!operations.empty())
+                numbers.push(compute(operations.pop(), numbers.pop(), numbers.pop()));
+            Double result = numbers.pop();
+            if (result == null) {
+                return null;
+            }
+            return result % 1 == 0 ? String.valueOf(result.intValue()) : Double.toString(result);
+        } catch (NumberFormatException | EmptyStackException e) {
+            return null;
+        }
     }
 
-    private double compute(char operation, double a, double b){
-        double result = 0;
+    private Double compute(char operation, double a, double b){
+        Double result = 0d;
         switch (operation){
             case '+':
                 result = a + b;
@@ -92,8 +88,11 @@ public class Calculator {
                 result = a * b;
                 break;
             case '/':
-//               TODO: / 0
-                result = b / a;
+                if (a == 0) {
+                    result = null;
+                } else {
+                    result = b / a;
+                }
                 break;
         }
         return result;
@@ -101,6 +100,12 @@ public class Calculator {
 
     private boolean isOperationPrioritized(char currentOperation, char peekedOperation) {
         if ((currentOperation == '-' || currentOperation == '+')
+                && (peekedOperation == '*' || peekedOperation == '/')) {
+            return true;
+        } else if ((currentOperation == '-' || currentOperation == '+')
+                && (peekedOperation == '-' || peekedOperation == '+')) {
+            return true;
+        } else if ((currentOperation == '*' || currentOperation == '/')
                 && (peekedOperation == '*' || peekedOperation == '/')) {
             return true;
         } else if ((currentOperation == '*' || currentOperation == '/')
